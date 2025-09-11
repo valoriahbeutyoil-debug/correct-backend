343e89c16ebded51f46b906d12b6e7f95e74988c
valoriahbeutyoil-debug <valoriahbeutyoil@gmail.com>
2025-09-11 10:56:10 +0100

    Add files via upload

--- PaymentMethod.js
+++ PaymentMethod.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 
 const PaymentMethodSchema = new mongoose.Schema({
-  type: { type: String, required: true },          // "Crypto", "Bank", "PayPal", "Skype"
-  credentials: { type: Object, default: {} },      // e.g. { bitcoin: "...", ethereum: "...", usdt: "..." }
+  type: { type: String, required: true }, // 'Crypto', 'PayPal', 'Bank', 'Skype'
+  credentials: { type: Object, required: true },
   active: { type: Boolean, default: true }
 }, { timestamps: true });
 
