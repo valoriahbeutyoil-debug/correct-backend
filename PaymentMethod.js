6f88320544cf185bc0c63c70f1d04841e1fa7149
valoriahbeutyoil-debug <valoriahbeutyoil@gmail.com>
2025-09-11 01:39:13 +0100

    Update PaymentMethod.js

--- PaymentMethod.js
+++ PaymentMethod.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 
 const PaymentMethodSchema = new mongoose.Schema({
-  type: { type: String, required: true }, // e.g. 'PayPal', 'Crypto', 'Bank'
-  credentials: { type: Object, default: {} }, // dynamic key/value pairs
+  type: { type: String, required: true },          // "Crypto", "Bank", "PayPal", "Skype"
+  credentials: { type: Object, default: {} },      // e.g. { bitcoin: "...", ethereum: "...", usdt: "..." }
   active: { type: Boolean, default: true }
 }, { timestamps: true });
 
